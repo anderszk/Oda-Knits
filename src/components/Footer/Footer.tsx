b7e000b02175a270c0ebea3794fef605fd48c1bb
Anders Kristensen <anders.kristensen@propulsentnu.no>
2024-05-09 20:40:49 +0200

    Footer finito (almost)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,11 +18,16 @@ const Footer = () => {
               <img src={logo} className='footer-logo-image'/>
             </Box>
           </Grid>
-          <Grid item md={10} xs={6}>
+          <Grid item md={8} xs={6}>
 
           </Grid>
+          <Grid item md={2} xs={6} sx={{display:'flex', flexDirection:'column', gap:'15px', justifyContent:'center'}}>
+            <NavLink to="/" className={({isActive}) => isActive ? "footer-link-active" : "footer-link"}>Home</NavLink>
+            <NavLink to="/projects" className={({isActive}) => isActive ? "footer-link-active" : "footer-link"}>Projects</NavLink>
+            <NavLink to="/contact" className={({isActive}) => isActive ? "footer-link-active" : "footer-link"}>Contact</NavLink>
+          </Grid>
         </Grid>
-        <Box className="footer-creds"> Designed and implemented by Kristensen Solutions © 2024</Box>
+        <Box className="footer-creds">Implemented by Kristensen Solutions © 2024</Box>
     </Box>
   );
 }
