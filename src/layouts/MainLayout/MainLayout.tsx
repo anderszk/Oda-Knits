23104e44f2ce3cd312756d09b0f0669bb83ba504
Anders Kristensen <anders.kristensen@propulsentnu.no>
2024-05-10 18:34:03 +0200

    feature:: typewriter

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,12 +1,33 @@
 
+import { Box, Typography } from '@mui/material';
 import './MainLayout.scss'
 
+import Typewriter from 'typewriter-effect';
+
 const MainLayout = ({ children } : {children: any}) => {
+
+  
   return (
     <div className="main-layout-container">
       <div className='main-landing'>
-        <h1>Hello! I am Oda Knits!</h1>
-        <p className="quote">"I like knitting stuff!" - <span style={{fontWeight:'600', fontSize:'.9em'}}>Dooda</span></p>
+        <Typography variant='h1' sx={{fontSize: {xs:'3rem', md:'4rem'}, fontWeight: '500'}}>Hello! I am Oda Knits!</Typography>
+        <Box mt={15}>
+          <Typewriter
+            options={{
+              strings: ["I Like knitting stuff!", "Follow me on instagram @ Oda.knits_!", "Take a look at my projects for inspo!"],
+              autoStart: true,
+              loop: true,
+              wrapperClassName: 'typewriter',
+              cursorClassName: 'typewriter_cursor'
+            }}
+            onInit={(typewriter) => {
+              typewriter.typeString('I Like knitting stuff!')
+                .pauseFor(2000)
+                .deleteAll()
+                .start();
+            }}
+          />
+        </Box>
       </div>
       <div className='main-content'>
         {children}
